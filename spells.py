65411f3149f6ee134c737682d16fb5b36d08a321
Brian <brian.chalfant@gmail.com>
2018-09-24 19:58:39 -1000

    updates 9-23

---
+++ spells.py
@@ -0,0 +1,180 @@
+def bard0():
+    spell_list = {
+        1: 'Blade Ward',
+        2: 'Dancing Lights',
+        3: 'Friends',
+        4: 'Light',
+        5: 'Mage Hand',
+        6: 'Mending',
+        7: 'Message',
+        8: 'Message',
+        9: 'Minor Illusion',
+        10: 'Prestidigitation',
+        11: 'True Strike'
+        }
+    return spell_list
+
+
+def bard1():
+    spell_list = {
+        1: 'Animal Friendship',
+        2: 'Bane',
+        3: 'Charm Person',
+        4: 'Comprehend Languages',
+        5: 'Cure',
+        6: 'Detect Magic',
+        7: 'Wounds',
+        8: 'Detect Magic',
+        9: 'Disguise Self',
+        10: 'Dissonant Whispers',
+        11: 'Faerie Fire',
+        12: 'Feather Fall',
+        13: 'Healing Word',
+        14: 'Heroism',
+        15: 'Identify',
+        16: 'Illusory Script',
+        17: 'Longstrider',
+        18: 'Silent Image',
+        19: 'Sleep',
+        20: 'Speak with Animals',
+        21: 'Tasha\'sHideous Laughter',
+        22: 'Thunderwave',
+        23: 'Unseen Servant'
+        }
+    return spell_list
+
+
+def bard2():
+    spell_list = {
+        1: 'Animal Messenger',
+        2: 'Blindness/Deafness',
+        3: 'Calm Emotions',
+        4: 'Cloud of Daggers',
+        5: 'Crown of Madness',
+        6: 'Detect Thoughts',
+        7: 'Enhance Ability',
+        8: 'Enthrall',
+        9: 'Heat Metal',
+        10: 'Hold Person',
+        11: 'Invisibility',
+        12: 'Knock',
+        13: 'Lesser Restoration',
+        14: 'LOcate Animals or Plants',
+        15: 'Locate Object',
+        16: 'Magic Mouth',
+        17: 'Phantasmal Force',
+        18: 'See Invisibility',
+        19: 'Shatter',
+        20: 'Silence',
+        21: 'Suggestion',
+        22: 'Zone of Truth'
+        }
+    return spell_list
+
+
+def bard3():
+    spell_list = {
+        1: 'Bestow Curse',
+        2: 'Clairvoyance',
+        3: 'Dispel Magic',
+        4: 'Fear',
+        5: 'Feign Death',
+        6: 'Glyph of Warding',
+        7: 'Hypnotic Pattern',
+        8: 'Leomund\'s Tiny Hut',
+        9: 'Major Image',
+        10: 'Nondetection',
+        11: 'Plant Growth',
+        12: 'Sending',
+        13: 'Speak with Dead',
+        14: 'Speak with Plants',
+        15: 'Stinking Cloud',
+        16: 'Tongues'
+        }
+    return spell_list
+
+
+def bard4():
+    spell_list = {
+        1: 'Compulsion',
+        2: 'Confusion',
+        3: 'Dimension Door',
+        4: 'Freedom of Movement',
+        5: 'Greater Invisibility',
+        6: 'Hallucinatory Terrain',
+        7: 'Locate Creature',
+        8: 'Polymorph'
+        }
+    return spell_list
+
+
+def bard5():
+    spell_list = {
+        1: 'Animate Objects',
+        2: 'Awaken',
+        3: 'Dominate Person',
+        4: 'Dream',
+        5: 'Geas',
+        6: 'Greater Restoration',
+        7: 'Hold Monster',
+        8: 'Legend Lore',
+        9: 'Mass Cure Wounds',
+        10: 'Mislead',
+        11: 'Modify Memory',
+        12: 'Planar Binding',
+        13: 'Raise Dead',
+        14: 'Scrying',
+        15: 'Seeming',
+        16: 'Teleportation Circle'
+        }
+    return spell_list
+
+
+def bard6():
+    spell_list = {
+        1: 'Eyebite',
+        2: 'Find the Path',
+        3: 'Guards and Wards',
+        4: 'Mass Suggestion',
+        5: 'Otto\'s Irresistible Dance',
+        6: 'Programmed Illusion',
+        7: 'True Seeing'
+        }
+    return spell_list
+
+
+def bard7():
+    spell_list = {
+        1: 'Etherealness',
+        2: 'Forcecage',
+        3: 'Mirage Arcane',
+        4: 'Mordenkainen\'s Magnificent Mansion',
+        5: 'Mordenkainen\'s Sword',
+        6: 'Project Image',
+        7: 'Regenerate',
+        8: 'Resurrection',
+        9: 'Symbol',
+        10: 'Teleport'
+        }
+    return spell_list
+
+
+def bard8():
+    spell_list = {
+        1: 'Dominate Monster',
+        2: 'Feeblemind',
+        3: 'Glibness',
+        4: 'Mind Blank',
+        5: 'Power Word Stun'
+        }
+    return spell_list
+
+
+def bard9():
+    spell_list = {
+        1: 'Foresight',
+        2: 'Power Word Heal',
+        3: 'Power Word Kill',
+        4: 'True Polymorph'
+        }
+    return spell_list
